201b46dcf1e210ecbb448d7426d2b495637d320b
Woojo Kim <kwoojo0127@gmail.com>
2021-02-28 00:36:17 +0900

    추가: HelloDto 클래스, 객체 응답 api 테스트 코드

--- src/test/java/springboot/web/HelloControllerTest.java
+++ src/test/java/springboot/web/HelloControllerTest.java
@@ -7,9 +7,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -23,7 +23,21 @@ public class HelloControllerTest {
         String hello = "hello";
 
         mvc.perform(get("/hello"))
-                .andExpect(status().isOk())
+                .andExpect(status().isOk()) // andExpect: mvc.perform 결과 검증
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                // $ 기준으로 필드명 명시
+                .andExpect(jsonPath("$.name", is(name))) // jsonPath: JSON 응답값 필드 별 검증 메소드
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
