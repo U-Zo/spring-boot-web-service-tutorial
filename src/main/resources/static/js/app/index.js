55a43e41cd4318a6b0c5906ae3b751bb47beacf8
Woojo Kim <kwoojo0127@gmail.com>
2021-03-22 03:56:55 +0900

    추가: 웹 템플릿 레이아웃, 메인, post 등록 화면

---
+++ src/main/resources/static/js/app/index.js
@@ -0,0 +1,29 @@
+const main = {
+  init() {
+    $('#btn-save').on('click', () => {
+      this.save();
+    });
+  },
+  save() {
+    const data = {
+      title: $('#title').val(),
+      author: $('#author').val(),
+      content: $('#content').val(),
+    };
+
+    $.ajax({
+      type: 'POST',
+      url: '/api/v1/posts',
+      dataType: 'json',
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify(data),
+    }).done(() => {
+      alert('글이 등록되었습니다.');
+      window.location.href = '/';
+    }).fail((err) => {
+      alert(JSON.stringify(err));
+    });
+  },
+};
+
+main.init();
